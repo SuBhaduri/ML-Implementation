2a9949c6841f3623e36ff24b98ae26bd6d8bfcd6
Subhamoy Bhaduri <62231240+SuBhaduri@users.noreply.github.com>
2020-11-06 18:56:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,24 @@ This is a temporary script file.
 import json
 import joblib
 import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
 #from azureml.core.model import Model
 
+app = Flask(__name__)
 print("subha 0")
+
+model = joblib.load("Kaggle_Titanic_RF_Azure_Deploy.pkl")
+print("subha 1")
 # Called when the service is loaded
-def init():
-    global model
+#def init():
+#    global model
     # Get the path to the registered model file and load it
-    model = joblib.load("Kaggle_Titanic_RF_Azure_Deploy.pkl")
-    print("subha 1")
+    
 
 # Called when a request is received
-def run(raw_data):
+@app.route('/predict_api',methods=['POST'])
+def predict_api(raw_data):
+#def run(raw_data):
     # Get the input data as a numpy array
     data = np.array(json.loads(raw_data)['data'])
     # Get a prediction from the model
